15d2037a57a50eb6a89cd2f1fef7a83b7f9171f7
Anastasiya Popova <anastasiya.popova@xperience.ai>
2019-09-24 11:59:53 +0300

    changed unittest to pytest

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 from itertools import product
 import torch
 from torchvision import models
-import unittest
+import pytest
 
 from gap_quantization.models.mobilenet import mobilenet_v2
 from gap_quantization.models.squeezenet import squeezenet1_0, squeezenet1_1
@@ -21,41 +21,36 @@ models_dict = {
     'resnext101_32x8d' : resnext101_32x8d,
 }
 
-class Tester(unittest.TestCase):
-
-    def _test_classification_model(self, name, input_shape):
-        # passing num_class equal to a number other than 1000 helps in making the test
-        # more enforcing in nature
-        model = models_dict[name](num_classes=50)
-        model.eval()
-        x = torch.rand(input_shape)
-        out = model(x)
-        self.assertEqual(out.shape[-1], 50)
+def _test_classification_model(name, input_shape):
+    # passing num_class equal to a number other than 1000 helps in making the test
+    # more enforcing in nature
+    model = models_dict[name](num_classes=50)
+    model.eval()
+    x = torch.rand(input_shape)
+    out = model(x)
+    assert out.shape[-1] == 50
 
 
-    def _test_equal(self, name, input_shape):
-        model1 = models_dict[name](pretrained=True, num_classes=50)
-        model1.eval()
-        x = torch.rand(input_shape)
-        out1 = model1(x)
-        model = models.__dict__[name](num_classes=50)
-        model.load_state_dict(model1.state_dict())
-        model.eval()
-        out2 = model(x)
-        self.assertListEqual(list(out1.detach().numpy().round(6).flatten()), list(out2.detach().numpy().round(6).flatten()))
+def _test_equal(name, input_shape):
+    model1 = models_dict[name](pretrained=True, num_classes=50)
+    model1.eval()
+    x = torch.rand(input_shape)
+    out1 = model1(x)
+    model = models.__dict__[name](num_classes=50)
+    model.load_state_dict(model1.state_dict())
+    model.eval()
+    out2 = model(x)
+    assert all(out1.detach().numpy().round(6).flatten() == out2.detach().numpy().round(6).flatten())
 
 
 
-for model_name in models_dict.keys():
-    # for-loop bodies don't define scopes, so we have to save the variables
-    # we want to close over in some way
-    def do_test(self, model_name=model_name):
-        input_shape = (1, 3, 224, 224)
-        self._test_classification_model(model_name, input_shape)
-        self._test_equal(model_name, input_shape)
+def test_models():
+    input_shape = (1, 3, 224, 224)
+    for model_name in models_dict.keys():
+        _test_classification_model(model_name, input_shape)
+        _test_equal(model_name, input_shape)
 
-    setattr(Tester, "test_" + model_name, do_test)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    test_models()
