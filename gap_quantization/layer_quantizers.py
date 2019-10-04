4a3cdbfbf14882ff0579ba9bdf379a86cdfb39f3
Maxim Zemlyanikin <maxim.zemlyanikin@xperience.ai>
2019-10-04 16:10:10 +0300

    Quantization fix

--- gap_quantization/layer_quantizers.py
+++ gap_quantization/layer_quantizers.py
@@ -16,16 +16,15 @@ def conv_quant(module, cfg):
         w_frac_bits -= out_int_bits + w_frac_bits + \
             inp_frac_bits - cfg['accum_bits'] + cfg['signed']
 
+    bias_frac_bits = cfg['bits'] - out_int_bits - cfg['signed']
     params = {
-        'norm':
-            max(0, out_int_bits + w_frac_bits + inp_frac_bits - cfg['bits'] + cfg['signed']),
-        'weight':
-            integerize(module.weight.data, w_frac_bits, cfg['bits']).cpu().tolist(),
-        'bias':
-            integerize(module.bias.data, cfg['bits'] - out_int_bits - cfg['signed'],
-                       cfg['bits']).cpu().tolist()
+        'norm': [max(0, out_int_bits + w_frac_bits + inp_frac_bits - cfg['bits'] + cfg['signed'])],
+        'weight': integerize(module.weight.data, w_frac_bits, cfg['bits']).cpu().tolist(),
+        'bias': integerize(module.bias.data, bias_frac_bits, cfg['bits']).cpu().tolist(),
+        '_w_frac_bits': [w_frac_bits],
+        '_b_frac_bits': [bias_frac_bits]
     }
-    params['dot_place'] = w_frac_bits + inp_frac_bits - params['norm']
+    params['dot_place'] = [w_frac_bits + inp_frac_bits - params['norm'][0]]
     return params
 
 
@@ -33,7 +32,7 @@ def concat_quant(module, cfg):
     min_int_bits = min(module.int_inp_bits)
     params = {
         'norm': [int_bits - min_int_bits for int_bits in module.int_inp_bits],
-        'dot_place': cfg['bits'] - min_int_bits - cfg['signed']
+        'dot_place': [cfg['bits'] - min_int_bits - cfg['signed']]
     }
     return params
 
