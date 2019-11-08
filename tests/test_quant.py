4022a8461f56667a38f5b7eacf63c1b1bd249277
Maxim Zemlyanikin <maxim.zemlyanikin@xperience.ai>
2019-11-08 19:35:19 +0300

    fix no grad

--- tests/test_quant.py
+++ tests/test_quant.py
@@ -51,12 +51,11 @@ def model():
 
 
 def test_conv_quant(model, transforms):
-    with torch.no_grad():
-        float_weight = model.conv2d.weight
-        float_bias = model.conv2d.bias
-        quantizer = ModelQuantizer(model, CFG, transforms)
-        quantizer.quantize_model()
-        rounded_weight = model.conv2d.weight * math.pow(2., -model.conv2d.w_frac_bits)
-        rounded_bias = model.conv2d.bias * math.pow(2., -model.conv2d.b_frac_bits)
+    float_weight = model.conv2d.weight
+    float_bias = model.conv2d.bias
+    quantizer = ModelQuantizer(model, CFG, transforms)
+    quantizer.quantize_model()
+    rounded_weight = model.conv2d.weight * math.pow(2., -model.conv2d.w_frac_bits)
+    rounded_bias = model.conv2d.bias * math.pow(2., -model.conv2d.b_frac_bits)
     assert np.allclose(float_weight.data.cpu().numpy(), rounded_weight.data.cpu().numpy(), atol=1e-2)
     assert np.allclose(float_bias.data.cpu().numpy(), rounded_bias.data.cpu().numpy(), atol=0.2)
